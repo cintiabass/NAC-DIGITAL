df9b01a3ce01536bb0b3648dbc78a632d3685690
cintiabass <mais_1_cintia@hotmail.com>
2020-09-25 22:05:12 -0300

    commit do projeto NAC_DIGITAL

---
+++ epictask-master/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO users (name, email, pass, GIT_HUB_USER) VALUES 
+('Cintia Cruz', 'rm83085@fiap.com.br', 'killmenow78', 'cintiabass');
+INSERT INTO tasks (title, description, point, status) VALUES
+('Tarefa','Tarefa de teste', 80, 50);
